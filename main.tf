5228dc746879fc07c9fdea2b3f62ed501da759cd
shindeshreas <shindeshreyas0211@gmail.com>
2026-09-26 20:51:15 +0530

    update repoURL in ingress-application.yaml to point to the correct repository

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@
 
 terraform {
   backend "s3" {
-    bucket = "mayurcbz.com"
+    bucket = "shreyasbuilds.fun"
     key    = "eks/terraform.tfstate"
-    region = "us-west-2"
+    region = "ap-south-1"
   }
 
   required_version = ">= 1.3"
@@ -26,7 +26,7 @@ data "aws_vpc" "default" { // Fetch default VPC
 
 variable "cluster_name" { // Variable for cluster name
   type    = string
-  default = "my-eks-cluster-mayur"
+  default = "my-eks"
 }
 
 data "aws_subnets" "default" { // Fetch  all subnets in the default VPC
